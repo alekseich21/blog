d11243955e09d3c7cbd61c0e4f73e617ac45ec57
Alexey Chuprin <chuprin.alex.q@gmail.com>
2021-12-06 12:48:27 +0200

    edit js1 file

--- general-mh-configs/index.js
+++ general-mh-configs/index.js
@@ -1,1 +1,9 @@
-let currentId;
+module.exports = {
+    // SP
+    'speedypaper.com': {
+        orderDataDefaults: {
+            academicLevel: 11,
+            deadline: 190,
+        },
+    }
+}
